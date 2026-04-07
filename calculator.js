3116c0f18c35a3ed80449580c167452e1d26d973
Alessandro Reis da Silva <alessandroreisdasilva6@gmail.com>
2026-04-07 14:25:23 -0300

    Correção do console.log

--- calculator.js
+++ calculator.js
@@ -3,26 +3,30 @@ function sum(v1, v2) {
     return v1 + v2;
 }
 
+console.log("Subtração" + sum(5, 3))
+
 // retorna o valor da subtração entre os dois parâmetros
 function sub(v1, v2) {
     return (v1 - v2)
 }
-console.log("Subtração" + (v1, v2))
+console.log("Subtração" + sub(5, 3))
 
 // retorna o valor da divisão entre os dois parâmetros
 function div(v1, v2) {
     return v1 / v2;
 }
+console.log("Divisão: " + div(3, 267));
 
 // retorna o valor da multiplicação entre os dois parâmetros
 function mult(v1, v2) {
     return v1 * v2;
 }
 
+console.log("Multplicação: " + mult(5, 2));
+
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
 function square(v1) {
     return Math.sqrt(v1)
 }
-console.log("Raiz Quadrada: " + (v1))
 
-console.log(sum(v1, v2));
+console.log("Raiz Quadrada: " + sum(2, 2));
